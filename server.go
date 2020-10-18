38ac2cb5aeda314f7de1c996a53ea2e800045797
pushlang <aapust@gmail.com>
2020-10-18 13:47:48 +0300

    initial

--- server.go
+++ server.go
@@ -1,16 +1,4 @@
-// +build OMIT
-
-// The server program issues Google search requests and demonstrates the use of
-// the go.net Context API. It serves on port 8080.
-//
-// The /search endpoint accepts these query params:
-//   q=the Google search query
-//   timeout=a timeout for the request, in time.Duration format
-//
-// For example, http://localhost:8080/search?q=golang&timeout=1s serves the
-// first few Google search results for "golang" or a "deadline exceeded" error
-// if the timeout expires.
-package main
+package cusearch
 
 import (
 	"context"
@@ -19,8 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	"golang.org/x/blog/content/context/google"
-	"golang.org/x/blog/content/context/userip"
+	"github.com/pushlang/cusearch/v2/google"
+	"github.com/pushlang/cusearch/v2/contxt"
 )
 
 func main() {
@@ -28,43 +16,28 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// handleSearch handles URLs like /search?q=golang&timeout=1s by forwarding the
-// query to google.Search. If the query param includes timeout, the search is
-// canceled after that duration elapses.
 func handleSearch(w http.ResponseWriter, req *http.Request) {
-	// ctx is the Context for this handler. Calling cancel closes the
-	// ctx.Done channel, which is the cancellation signal for requests
-	// started by this handler.
 	var (
 		ctx    context.Context
 		cancel context.CancelFunc
 	)
 	timeout, err := time.ParseDuration(req.FormValue("timeout"))
 	if err == nil {
-		// The request has a timeout, so create a context that is
-		// canceled automatically when the timeout expires.
 		ctx, cancel = context.WithTimeout(context.Background(), timeout)
 	} else {
 		ctx, cancel = context.WithCancel(context.Background())
 	}
-	defer cancel() // Cancel ctx as soon as handleSearch returns.
+	defer cancel()
 
-	// Check the search query.
 	query := req.FormValue("q")
 	if query == "" {
 		http.Error(w, "no query", http.StatusBadRequest)
 		return
 	}
 
-	// Store the user IP in ctx for use by code in other packages.
-	userIP, err := userip.FromRequest(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	ctx = userip.NewContext(ctx, userIP)
+	cx := "a47264a82f21a8911"
+	ctx = contxt.NewContext(ctx, cx)
 
-	// Run the Google search and print the results.
 	start := time.Now()
 	results, err := google.Search(ctx, query)
 	elapsed := time.Since(start)
